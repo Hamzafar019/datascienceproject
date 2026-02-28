7fdcf878927d026a52f01c2997421e59474dd01d
Hamzafar019 <mhz15019@gmail.com>
2026-02-28 05:06:31 +0500

    Data ingestion Part 2

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from src.datascience import logger
+from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingpipeline
 
-logger.info("Welcome to our custom logging data science")
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<")
+    data_ingestion = DataIngestionTrainingpipeline()
+    data_ingestion.initiate_data_ingestion()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
